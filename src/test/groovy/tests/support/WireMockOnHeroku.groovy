d055de92ddc7c1026a17c8ade353a35d69ea158c
RobeEW <robe@energizedwork.com>
2015-10-23 13:36:11 +0100

    Made checking on Wiremock more resilient to timeouts - heroku can be sloooow

--- src/test/groovy/tests/support/WireMockOnHeroku.groovy
+++ src/test/groovy/tests/support/WireMockOnHeroku.groovy
@@ -2,6 +2,9 @@ package tests.support
 
 import client.HttpClient
 import com.google.common.io.Files
+import jodd.http.HttpException
+
+import java.util.concurrent.TimeUnit
 
 import static Exec.exec
 
@@ -9,7 +12,7 @@ class WireMockOnHeroku {
 
     static void setup(String appName) {
         def client = new HttpClient("http://${appName}.herokuapp.com/")
-        if (client.get("/__admin/").statusCode() == 404) {
+        if (wiremockExists(client)) {
             def tempdir = Files.createTempDir()
             tempdir.deleteOnExit()
             exec(tempdir, "git clone git@github.com:Mahoney/wiremock-heroku.git")
@@ -20,6 +23,19 @@ class WireMockOnHeroku {
                 Runtime.runtime.addShutdownHook {
                     exec("heroku apps:destroy --app $appName --confirm $appName")
                 }
+            }
+        }
+    }
+
+    private static boolean wiremockExists(HttpClient client, int retries = 0) {
+        try {
+            return client.get("/__admin/").statusCode() == 404
+        } catch (HttpException e) {
+            if (retries < 5) {
+                TimeUnit.SECONDS.sleep(1);
+                return wiremockExists(client, retries + 1)
+            } else {
+                throw e
             }
         }
     }
